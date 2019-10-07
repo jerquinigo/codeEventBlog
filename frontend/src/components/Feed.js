ca2df209c8382cb6f93fd0587a3693b5466f6b41
xpectro93 <jonathanandrade@pursuit.org>
2019-10-07 17:11:46 -0400

    Init Feed and LogIn Components

---
+++ frontend/src/components/Feed.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class Feed extends Component {
+
+    state = {
+
+    }
+    render(){
+        return ( <h1>This b Feed</h1>)
+    }
+};
+
+
+
+
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Login extends Component {
+
+    state = {
+
+    }
+    render(){
+        return ( <h1>We are going to log in and all that shiz through this</h1>)
+    }
+};
