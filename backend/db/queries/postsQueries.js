401d94af0376c95c37492e7e34f739f374359c28
xpectro93 <jonathanandrade@pursuit.org>
2019-10-08 14:11:36 -0400

    add getPost

--- backend/db/queries/postsQueries.js
+++ backend/db/queries/postsQueries.js
@@ -1,7 +1,18 @@
 const { db } = require('.');
 
 const getPostByUserId = ( req, res, next) => {
-    db.one('SELECT id, threads_id, user_id, posts_code, posts_comment FROM posts where user_id =$1')
+    db.any('SELECT posts.id AS post_id , threads_id, user_id,username, profile_pic, posts_code, posts_comment FROM posts JOIN users ON users.id = posts.user_id WHERE user_id = $1')
+    .then(posts => {
+        res.status(200).json({
+            status: "success",
+			posts:posts,
+			message: "got all the threads"
+        })
+    })
+    .catch(err =>{
+        console.log(err);
+		return next(err);
+     })
 }
 
 module.exports = {
