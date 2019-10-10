573f79758b5b5e16be442844ebca7e6aac6b7ccd
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-10-10 17:00:43 -0400

    updated getallthreads

--- backend/db/queries/threadsQueries.js
+++ backend/db/queries/threadsQueries.js
@@ -1,7 +1,7 @@
 const { db } = require("../index.js");
 
 getAllThreads = (req, res, next) => {
-	db.any("SELECT user_id, threads_code,threads_comment FROM threads")
+	db.any("SELECT user_id, threads.id AS thread_id, threads_code,threads_comment FROM threads")
 		.then(threads => {
 			res.status(200).json({
 				status: "success",
