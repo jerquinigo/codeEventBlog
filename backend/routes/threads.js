632287d01d2f33ed11d9973011dae2282028ebe1
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-10-10 16:53:36 -0400

    added query

--- backend/routes/threads.js
+++ backend/routes/threads.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const {getAllThreads, getThreadsByUserId, deleteThread} = require("../db/queries/threadsQueries.js")
+const {getAllThreads, getThreadsByUserId, deleteThread, createThreadById} = require("../db/queries/threadsQueries.js")
 
 
 router.get("/", getAllThreads);
 router.get("/:id", getThreadsByUserId)
 router.delete("/:id", deleteThread)
+router.post("/:id", createThreadById)
 
 
 
