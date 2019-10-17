826091da3dac659bf4ac632331a7473fbb0b0004
Treagan Birbal <treaganbirbal@pursuit.org>
2019-10-17 18:24:09 -0400

    implented login button to login form

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -5,9 +5,14 @@ const LoginForm = () => {
         <div className='login_form'>
         <h1>Log In</h1>
         <form >
-            <span className='email'>email
+            <span className='email'>
                 Email: <input value='email' name='email' type="text"/>
+            </span> 
+            <span className='password'>   
                 Password: <input value='password' name='passworl' type="password"/>
+            </span>
+            <span>
+             <input className='login_btn' value='Log In' type="submit"/>
             </span>
         </form>
         </div>
