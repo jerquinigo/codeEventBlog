35d81fcbe07f072d6fa6d94ce382a5ce2258618f
Treagan Birbal <treaganbirbal@pursuit.org>
2019-10-17 18:19:43 -0400

    email and password fields for form

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const LoginForm = () => {
     return (
+        <div className='login_form'>
         <h1>Log In</h1>
+        <form >
+            <span className='email'>email
+                Email: <input value='email' name='email' type="text"/>
+                Password: <input value='password' name='passworl' type="password"/>
+            </span>
+        </form>
+        </div>
     )
 }
 
