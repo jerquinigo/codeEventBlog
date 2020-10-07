26541dc84d5e6dbc4bd75a789b0c460d4b922523
Jonathan Andrade <43793627+xpectro93@users.noreply.github.com>
2020-10-07 19:10:14 -0400

    add user names to feed

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 
-export default function Feed () {
-
+export default function Feed (props) {
+        let names = props.users.map(user => {
+        return ( <div> {user.name} </div>)
+        
+        })
         return ( 
        <div className='feed'>
             <div className='main'>
+                
+                
                 <h1>This b Feed</h1>
+                {names}
             </div>
           
         </div>
