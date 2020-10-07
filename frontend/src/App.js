63421e379840ed6cc79bd5ede2ec82805ea1a4a2
Jonathan Andrade <43793627+xpectro93@users.noreply.github.com>
2020-10-07 17:28:57 -0400

    Update App.js to use Hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom'
 //Component Imports
 import Feed from './components/Feed';
@@ -6,44 +6,18 @@ import Login from './components/Login';
 import {PrivateRoute} from './utils/AuthRouting.js'
 
 import './App.css';
-
-class App extends Component {
-
-  state = {
-    test:""
-  }
-
-  render(){
+ex[prt default function App {
+   const [ state, setState ] = useState(null);
 
   return (
+   
     <div className="App">
       <Switch>
-      <Route path='/login' component={Login} />
-      <PrivateRoute exact path="/feed" component={Feed}/>
-
-
+        <Route path='/login' component={Login} />
+        <PrivateRoute exact path="/feed" component={Feed}/>
       </Switch>
       
     </div>
-  );
-}
+  )
 
 }
-
-export default App;
-
-
-{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-</header> */}
