ac8a0282f8b47d1a42833679d412c8f5213fa7e6
Jonathan Andrade <43793627+xpectro93@users.noreply.github.com>
2020-10-07 17:43:13 -0400

    Update state to use hooks

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React , { useState } from 'react';
 import SignUpForm from './SignUpForm.js';
 
-class SignUp extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            email : '',
-            username: '',
-            password: '',
-            submitted: false,
-        }
-    }
-  render(){
-      const {email, username, password, submitted} = this.state;
+export default function SignUp() {
+
+ const [email, setEmail ] = useState("");
+ const [username, setUsername ] = useState("");
+ const [password, setPassword ] = useState("");
+ const [submitted, setSubmitter ] = useState(false);
+
       return(
           <div className='signUp'>
               <SignUpForm email={email} username={username} password={password} submitted={submitted}/>
           </div>
       )
-  }
+
 
 }
 
-export default SignUp;
